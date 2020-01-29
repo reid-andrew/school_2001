9b68de7b48ac723b9ea211e5c619be95bdc52306
Andrew Reid <areid.sj@gmail.com>
2020-01-29 13:10:43 -0700

    Pass starts with no students test

--- test/school_test.rb
+++ test/school_test.rb
@@ -12,21 +12,21 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_has_start_time
-    # skip
+    skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    # skip
+    skip
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
+    # skip
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
